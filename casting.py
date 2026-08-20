49e7191f957d64bc66387329faea0f122afab3ba
maha0330 <maha03302007@gmail.com>
2026-08-20 22:16:16 +0530

    Add casting program

---
+++ casting.py
@@ -0,0 +1,18 @@
+name="Maha"
+age="19"
+height=154
+weight=55
+marks="500"
+#convert string to int
+age=int(age)
+#covert int to float
+height=float(height)       
+#convert str to float
+mark=float(marks)
+#convert int to str
+weight=str(weight)
+print(age)
+print(height)
+print(mark)
+print(weight)
+                   

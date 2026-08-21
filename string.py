9df6e4ab197ad4a438d2631d8ff927f96a9faea7
maha0330 <maha03302007@gmail.com>
2026-08-21 09:44:52 +0530

    string

---
+++ string.py
@@ -0,0 +1,11 @@
+# String program
+name = "Mahalakshmi"
+print("Name:", name)
+print("Length:", len(name))
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+print("First character:", name[0])
+print("Last character:", name[-1])
+print("First 5 characters:", name[:5])
+print("Is 'a' present?", "a" in name)
+print("Replaced:", name.replace("a", "A"))
